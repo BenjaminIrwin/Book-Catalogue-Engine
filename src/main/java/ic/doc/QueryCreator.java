f1eb831381001a779718cb644785ee3207e73b89
bti19 <bti19@ic.ac.uk>
2020-02-20 15:20:34 +0000

    BP BI - Wrote QueryCreator member variables

--- src/main/java/ic/doc/QueryCreator.java
+++ src/main/java/ic/doc/QueryCreator.java
@@ -2,6 +2,26 @@ package ic.doc;
 
 public class QueryCreator {
 
+  private String name1 = null;
+  private String name2 = null;
+  private String title = null;
+  private Integer date1 = null;
+  private Integer date2 = null;
+;
+ private QueryCreator() {};
 
+ public static QueryCreator aQuery() {
+   return new QueryCreator();
+ }
+
+ public BookSearchQuery search() {
+   BookSearchQuery query = new BookSearchQuery(name1, name2, title, date1, date2);
+   return query;
+ }
+
+  public QueryCreator firstName(String name) {
+   name1 = name;
+   return this;
+  }
 
 }
